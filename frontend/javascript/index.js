5841a9c9d15995f066f5f461cd5bccd3cd2b9d1a
Daniel Schömer <daniel.schoemer@gmx.net>
2022-03-25 19:51:21 +0000

    Add Bridgetown Turbo

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,4 +1,12 @@
 import "index.css"
+import * as Turbo from "@hotwired/turbo"
+
+// Uncomment the line below to add transition animations when Turbo navigates.
+// We recommend adding <meta name="turbo-cache-control" content="no-preview" />
+// to your HTML head if you turn on transitions. Use data-turbo-transition="false"
+// on your <main> element for pages where you don't want any transition animation.
+//
+// import "./turbo_transitions.js"
 
 // Import all JavaScript & CSS files from src/_components
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
