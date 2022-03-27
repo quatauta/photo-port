2596f0b7c68860d3aa96761ebbc625e0d18113e4
Daniel Schömer <daniel.schoemer@gmx.net>
2022-03-27 19:32:52 +0000

    Purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,22 @@
 module.exports = {
   plugins: {
     'tailwindcss': {},
+    'postcss-easy-import': {},
+    'postcss-mixins': {},
+    'postcss-color-function': {},
     'postcss-flexbugs-fixes': {},
     'postcss-preset-env': {
       autoprefixer: {
         flexbox: 'no-2009'
       },
-      stage: 3
+      stage: 4,
+      features: {
+        'nesting-rules': true,
+        'custom-media-queries': true
+      },
+    },
+    'cssnano' : {
+      preset: 'default'
     }
   }
 }
