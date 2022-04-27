b98d16fe1a4918dcf3b34e8947fd285dc96b778a
Daniel Schömer <daniel.schoemer@gmx.net>
2022-04-27 20:34:42 +0000

    Cleanup Flickr builder

--- plugins/builders/flickr_portfolio.rb
+++ plugins/builders/flickr_portfolio.rb
@@ -1,3 +1,4 @@
+require "date"
 require "dotenv/load"
 require "flickr"
 
@@ -43,95 +44,46 @@ class Builders::FlickrPortfolio < SiteBuilder
     </div>
   EOS
 
-  # use JSON file cache like bin/flickr
   def cache
     @@cache ||= Bridgetown::Cache.new(self.class.to_s)
   end
 
   def flickr_photosets(flickr, user_id)
-    flickr.photosets.getList(user_id: user_id)["photoset"]
+    cache.getset("user_#{user_id}_photosets_#{Date.today}") do
+      puts "fetching #{user_id} photoset list"
+      flickr.photosets.getList(user_id: user_id)["photoset"]
+    end
   end
 
   def flickr_photoset_photos(flickr, user_id, photoset)
-    # use JSON file cache like bin/flickr
-    cache_key = "flickr_photoset_#{photoset["id"]}_photos_#{photoset["date_update"]}"
-    cache.getset(cache_key) do
+    cache.getset("photoset_#{photoset["id"]}_photos_#{photoset["date_update"]}") do
       puts "fetching #{user_id} photoset #{photoset["id"]} photo list"
       flickr.photosets.getPhotos(user_id: user_id, photoset_id: photoset["id"], media: :photos, extras: "last_update")["photo"]
     end
   end
 
   def flickr_photo(flickr, photo)
-    id = photo["id"]
-    secret = photo["secret"]
-    last_update = photo["last_update"]
-
-    # use JSON file cache like bin/flickr
-    cache_key = "flickr_photo_#{id}_#{last_update}"
-    cache.getset(cache_key) do
-      puts "fetching photo #{id} info"
-      info = flickr.photos.getInfo(photo_id: id, secret: secret)
-
-      info
+    cache.getset("photo_#{photo["id"]}_info_#{photo["last_update"]}") do
+      puts "fetching photo #{photo["id"]} info"
+      flickr.photos.getInfo(photo_id: photo["id"], secret: photo["secret"])
     end
   end
 
   def flickr_photo_exif(flickr, photo)
-    id = photo["id"]
-    secret = photo["secret"]
-    last_update = photo["last_update"]
-
-    # use JSON file cache like bin/flickr
-    cache_key = "flickr_photo_exif_#{id}_#{last_update}"
-    cache.getset(cache_key) do
-      exif = nil
-
-      begin
-        puts "fetching photo #{id} exif"
-        exif = flickr.photos.getExif(photo_id: id, secret: secret)
-      rescue
-      end
-
-      exif && exif["exif"].map { |value| value.respond_to?(:to_hash) ? value.to_hash : value }
+    cache.getset("photo_#{photo["id"]}_exif_#{photo["last_update"]}") do
+      puts "fetching photo #{photo["id"]} exif"
+      flickr.photos.getExif(photo_id: photo["id"], secret: photo["secret"])
+    rescue
+      # getExif API call with fail with error if exif information is not available
     end
   end
 
   def flickr_photo_location(flickr, photo)
-    id = photo["id"]
-    secret = photo["secret"]
-    last_update = photo["last_update"]
-
-    # use JSON file cache like bin/flickr
-    cache_key = "flickr_photo_location_#{id}_#{last_update}"
-    cache.getset(cache_key) do
-      location = nil
-
-      begin
-        puts "fetching photo #{id} location"
-        location = flickr.photos.geo.getLocation(photo_id: id, secret: secret)
-      rescue
-      end
-
-      location && location["location"].to_hash
-    end
-  end
-
-  def flickr_photo_sizes(flickr, photo)
-    id = photo["id"]
-    last_update = photo["last_update"]
-
-    # use JSON file cache like bin/flickr
-    cache_key = "flickr_photo_sizes_#{id}_#{last_update}"
-    cache.getset(cache_key) do
-      sizes = nil
-
-      begin
-        puts "fetching photo #{id} sizes"
-        sizes = flickr.photos.getSizes(photo_id: id)
-      rescue
-      end
-
-      sizes && sizes["size"].map { |value| value.to_hash }
+    cache.getset("photo_#{photo["id"]}_location_#{photo["last_update"]}") do
+      puts "fetching photo #{photo["id"]} location"
+      flickr.photos.geo.getLocation(photo_id: photo["id"], secret: photo["secret"])
+    rescue
+      # getLocation API call with fail with error if location information is not available
     end
   end
 
@@ -139,20 +91,7 @@ class Builders::FlickrPortfolio < SiteBuilder
     photoset["title"].sub(/\s*portfolio\s*/i, "").parameterize
   end
 
-  def photo_slug(photo, index)
-    photo["title"].parameterize
-  end
-
-  def portfolio_photo_resource(set, photo, index)
-    "#{index + 1}-#{photo_slug(photo, index)}.md"
-  end
-
-  def portfolio_resource(set)
-    "#{portfolio_slug(set)}.md"
-  end
-
   def build
-    builder_id = "builder://#{self.class.to_s.sub("::", ".")}"
     key = ENV["FLICKR_KEY"]
     secret = ENV["FLICKR_SECRET"]
     user = ENV["FLICKR_USER"]
@@ -162,17 +101,9 @@ class Builders::FlickrPortfolio < SiteBuilder
     photosets = flickr_photosets(flickr, user)
 
     photosets.select { |set| set["title"] =~ /portfolio/i }.each do |set|
-      frontmatter = {
-        layout: "default",
-        pagination: {
-          collection: portfolio_slug(set),
-          per_page: 10,
-          sort_field: "relative_url",
-          sort_reverse: false
-        },
-      }
-      add_resource :pages, portfolio_resource(set) do
-        ___ frontmatter
+      add_resource :pages, "#{portfolio_slug(set)}.md" do
+        layout "default"
+        pagination from: -> { {collection: portfolio_slug(set), per_page: 10, sort_field: "relative_url", sort_reverse: false} }
         content PORTFOLIO_CONTENT
       end
 
@@ -180,14 +111,8 @@ class Builders::FlickrPortfolio < SiteBuilder
 
       photoset_photos.each_with_index do |photo, index|
         info = flickr_photo(flickr, photo)
-        exif = flickr_photo_exif(flickr, photo)
-
-        prev_index = (index - 1) % photoset_photos.size
-        next_index = (index + 1) % photoset_photos.size
-        prev_photo = photoset_photos[prev_index]
-        next_photo = photoset_photos[next_index]
-        prev_id = "#{builder_id}/#{portfolio_photo_resource(set, prev_photo, prev_index)}"
-        next_id = "#{builder_id}/#{portfolio_photo_resource(set, next_photo, next_index)}"
+        exif = flickr_photo_exif(flickr, photo)&.to_hash&.fetch("exif", nil)
+        location = flickr_photo_location(flickr, photo)&.to_hash&.fetch("location", nil)
 
         frontmatter = {
           layout: "page",
@@ -197,13 +122,12 @@ class Builders::FlickrPortfolio < SiteBuilder
           flickr_id: info["id"],
           flickr_url: Flickr.url_photopage(info),
           source: Flickr.url_o(info),
-          info: info.to_hash,
+          info: info&.to_hash,
           exif: exif,
-          prev: prev_id,
-          next: next_id
+          location: location&.to_hash
         }
 
-        add_resource portfolio_slug(set), portfolio_photo_resource(set, info, index) do
+        add_resource portfolio_slug(set), "#{index + 1}-#{info["title"].parameterize}.md" do
           ___ frontmatter
           content PHOTO_CONTENT
         end
